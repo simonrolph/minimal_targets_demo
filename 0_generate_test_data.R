264949b36364f084421aacedf9239cf5ba0db16c
Simon Rolph <simon.p.rolph@gmail.com>
2024-02-15 16:28:55 +0000

    first commit

---
+++ 0_generate_test_data.R
@@ -0,0 +1,13 @@
+#create 
+dir.create("inputs")
+dir.create("outputs")
+
+x <- sample(1:100,100,replace = T)
+y <- x*2+sample(1:50,100,replace = T)
+
+df <- data.frame(x = x, y = y)
+write.csv(df,"inputs/data.csv",row.names = F)
+
+rm(df)
+rm(x)
+rm(y)
---
+++ 1_non_targets_version.R
@@ -0,0 +1,24 @@
+#load the data
+df <- read.csv("inputs/data.csv")
+
+#what's the mean of each value
+stat1 <- colMeans(df)
+
+# fit a linear model
+mod1 <- lm(y~x,df)
+
+#some new data to make predictions from
+newdata1 <- data.frame(x=20:60)
+
+#making a prediction
+pred1 <- data.frame(x = newdata1$x,
+                    y = predict(mod1,newdata = newdata1))
+
+#save a plot ("outputs/plot.png") with a line on it and a title
+png(file="outputs/plot.png",width=600, height=350)
+plot(df)
+lines(pred1,col="red")
+title("My nice plot")
+dev.off()
+
+
